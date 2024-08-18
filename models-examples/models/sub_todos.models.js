60873588857c7852a403a52fb5ea3fa87cb5af91
gulshanpr <gulshankumarprasad04@gmail.com>
2024-08-18 15:51:49 +0530

    03:06:25

---
+++ models-examples/models/sub_todos.models.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const subTodos = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true });
+
+export const SubTodo = mongoose.model("SubTodo", subTodos);
